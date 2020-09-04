36b93a5c73f34fbe38f24633794e4314723f99ca
Mamadou MAREGA <hamshakour93@gmail.com>
2020-09-04 18:56:54 +0200

    Add show pages links between pages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pins;
 use App\Repository\PinsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
@@ -11,9 +13,17 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(PinsRepository $pinsRepository)
+    public function index(PinsRepository $pinsRepository): Response
     {
         $pins = $pinsRepository->findAll();
         return $this->render('pins/index.html.twig',compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show(Pins $pin)
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
