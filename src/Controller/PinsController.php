ea5770f3fefde2e9d956748d9f20a007739ce4eb
Mamadou MAREGA <hamshakour93@gmail.com>
2020-09-05 19:14:46 +0200

    add delete feature

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -72,6 +72,19 @@ class PinsController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods="DELETE")
+     */
+
+    public function delete(Pins $pin, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($pin);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+
+    }
+
 
     /**
      * @Route("/pins/{id<[0-9]+>}", name="app_pins_show",  methods="GET")
