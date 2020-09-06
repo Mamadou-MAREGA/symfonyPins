c1e6ba88dd1b62df7f378f576d24dec8fa49308b
Mamadou MAREGA <hamshakour93@gmail.com>
2020-09-06 18:02:36 +0200

    add validation

--- src/Entity/Pins.php
+++ src/Entity/Pins.php
@@ -7,6 +7,7 @@ use App\Repository\PinsRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ORM\Mapping\HasLifecycleCallBacks;
 use App\Entity\Traits\Timestampable;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinsRepository::class)
@@ -25,11 +26,15 @@ class Pins
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le titre ne peut pas être vide")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     *  @Assert\NotBlank(message="La description ne peut pas être vide")
+     * @Assert\Length(min=10, max=200, minMessage="Le champ doit avoir 10 caractères minumum",
+     *     maxMessage="Le champ ne doit pas dépasser 200 caractères ")
      */
     private $description;
 
